8658781b44fc1108e087f94b0b7aec2d7c00b24b
Twelve <1732756017@qq.com>
2021-12-17 19:04:14 +0800

    add file

--- editor/cn/[5]最长回文子串.cpp
+++ editor/cn/[5]最长回文子串.cpp
@@ -48,7 +48,48 @@ class Solution {
 public:
     // 最长回文子串
     string longestPalindrome(string s) {
+        int n = s.size();
+        if (n < 2) {
+            return s;
+        }
 
+        // dp table
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+
+        // base case
+        for (int i=0; i<n; i++) {
+            dp[i][i] = 1;
+        }
+
+        int start = 0, max = 1;
+        // 状态转换方程
+        // f(i,j) = { 1, len<2; len = j-i+1 }
+        //        = { 1, s[i] == s[j] && f(i+1, j-1) == 1;
+        //            else 0; len >= 2 }
+        // for loop
+        // 遍历所有子串
+        for (int j=1; j<n; j++) {
+            for (int i=0; i<j; i++) {
+
+                // 左右边界字符相等
+                if (s[i] == s[j]) {
+                    // (j-1) - (i+1) + 1 < 2 ==> j-i < 3
+                    if (j-i < 3) { // 保证base case 满足
+                        dp[i][j] = 1;
+                    } else {
+                        dp[i][j] = dp[i+1][j-1];
+                    }
+                }
+
+                // 求结果
+                if (dp[i][j] == 1 && j-i+1 > max) {
+                    start = i;
+                    max = j-i+1;
+                }
+            }
+        }
+
+        return s.substr(start, max);
     }
 };
 //leetcode submit region end(Prohibit modification and deletion)
