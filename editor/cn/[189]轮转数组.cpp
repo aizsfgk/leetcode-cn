1ac31902085ca7941d61f676e5dd05004cf94f36
aizsfgk <1732756017@qq.com>
2022-02-24 10:33:35 +0800

    add file

---
+++ editor/cn/[189]轮转数组.cpp
@@ -0,0 +1,68 @@
+//给你一个数组，将数组中的元素向右轮转 k 个位置，其中 k 是非负数。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [1,2,3,4,5,6,7], k = 3
+//输出: [5,6,7,1,2,3,4]
+//解释:
+//向右轮转 1 步: [7,1,2,3,4,5,6]
+//向右轮转 2 步: [6,7,1,2,3,4,5]
+//向右轮转 3 步: [5,6,7,1,2,3,4]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：nums = [-1,-100,3,99], k = 2
+//输出：[3,99,-1,-100]
+//解释: 
+//向右轮转 1 步: [99,-1,-100,3]
+//向右轮转 2 步: [3,99,-1,-100] 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= nums.length <= 105 
+// -231 <= nums[i] <= 231 - 1 
+// 0 <= k <= 105 
+// 
+//
+// 
+//
+// 进阶： 
+//
+// 
+// 尽可能想出更多的解决方案，至少有 三种 不同的方法可以解决这个问题。 
+// 你可以使用空间复杂度为 O(1) 的 原地 算法解决这个问题吗？ 
+// 
+//
+// 
+// 
+//
+// 
+// 
+// Related Topics 数组 数学 双指针 
+// 👍 1343 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    void rotate(vector<int>& nums, int k) {
+        int n = nums.size();
+        if (k % n == 0) return;
+
+        vector<int> tmp(n);
+        for (int i=0; i<n; i++) {
+            tmp[(i+k) % n] = nums[i]; // (i+k) % n
+        }
+
+        nums.assign(tmp.begin(), tmp.end());
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
