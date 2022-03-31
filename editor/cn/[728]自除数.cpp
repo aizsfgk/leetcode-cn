4e246bee025d2ad95ecafe8cc083d96e06ec604e
aizsfgk <1732756017@qq.com>
2022-03-31 15:01:19 +0800

    add some file

---
+++ editor/cn/[728]自除数.cpp
@@ -0,0 +1,62 @@
+//自除数 是指可以被它包含的每一位数整除的数。 
+//
+// 
+// 例如，128 是一个 自除数 ，因为 128 % 1 == 0，128 % 2 == 0，128 % 8 == 0。 
+// 
+//
+// 自除数 不允许包含 0 。 
+//
+// 给定两个整数 left 和 right ，返回一个列表，列表的元素是范围 [left, right] 内所有的 自除数 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//输入：left = 1, right = 22
+//输出：[1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 15, 22]
+// 
+//
+// 示例 2: 
+//
+// 
+//输入：left = 47, right = 85
+//输出：[48,55,66,77]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= left <= right <= 104 
+// 
+// Related Topics 数学 
+// 👍 171 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    vector<int> selfDividingNumbers(int left, int right) {
+        vector<int> ans;
+        for (int i=left; i<=right; i++) {
+            if (isSelfDividingNumber(i)) {
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+
+    bool isSelfDividingNumber(int num) {
+        int tmp = num;
+        while (tmp) {
+            int one = tmp % 10;
+            if (one == 0 || num % one != 0)
+                return false;
+            tmp /= 10;
+        }
+        return true;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
