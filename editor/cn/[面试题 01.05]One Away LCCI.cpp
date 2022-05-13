32835d2d81f3d594a519b0f98a6d952e4f6ba69a
aizsfgk <1732756017@qq.com>
2022-05-13 19:37:38 +0800

    add some file

---
+++ editor/cn/[面试题 01.05]One Away LCCI.cpp
@@ -0,0 +1,35 @@
+//There are three types of edits that can be performed on strings: insert a char
+//acter, remove a character, or replace a character. Given two strings, write a fu
+//nction to check if they are one edit (or zero edits) away. 
+//
+// 
+//
+// Example 1: 
+//
+// 
+//Input: 
+//first = "pale"
+//second = "ple"
+//Output: True
+// 
+//
+// Example 2: 
+//
+// 
+//Input: 
+//first = "pales"
+//second = "pal"
+//Output: False
+// 
+// Related Topics 双指针 字符串 
+// 👍 188 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    bool oneEditAway(string first, string second) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
---
+++ editor/cn/[面试题 01.05]一次编辑.cpp
@@ -0,0 +1,32 @@
+//字符串有三种编辑操作:插入一个字符、删除一个字符或者替换一个字符。 给定两个字符串，编写一个函数判定它们是否只需要一次(或者零次)编辑。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: 
+//first = "pale"
+//second = "ple"
+//输出: True 
+//
+// 
+//
+// 示例 2: 
+//
+// 输入: 
+//first = "pales"
+//second = "pal"
+//输出: False
+// 
+// Related Topics 双指针 字符串 
+// 👍 188 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+public:
+    bool oneEditAway(string first, string second) {
+
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
