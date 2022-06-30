242cce245d1fda30965383b038c7c397befc5ac5
aizsfgk <1732756017@qq.com>
2022-06-30 19:58:26 +0800

    add 展平二叉树

---
+++ editor/cn/[剑指 Offer II 052]展平二叉搜索树.cpp
@@ -0,0 +1,87 @@
+//给你一棵二叉搜索树，请 按中序遍历 将其重新排列为一棵递增顺序搜索树，使树中最左边的节点成为树的根节点，并且每个节点没有左子节点，只有一个右子节点。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：root = [5,3,6,2,4,null,8,1,null,null,null,7,9]
+//输出：[1,null,2,null,3,null,4,null,5,null,6,null,7,null,8,null,9]
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：root = [5,1,7]
+//输出：[1,null,5,null,7]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 树中节点数的取值范围是 [1, 100] 
+// 0 <= Node.val <= 1000 
+// 
+//
+// 
+//
+// 注意：本题与主站 897 题相同： https://leetcode-cn.com/problems/increasing-order-search-
+//tree/ 
+// Related Topics 栈 树 深度优先搜索 二叉搜索树 二叉树 👍 34 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+
+public:
+    TreeNode* increasingBST(TreeNode* root) {
+        //
+        // 中序遍历的非递归版本
+        //
+
+        if (!root) return nullptr;
+
+        stack<TreeNode *> stk;
+        TreeNode *first = nullptr;
+        TreeNode *prev = nullptr;
+        // 中序遍历页游非递归的方式
+        while (root != nullptr || !stk.empty()) {
+            while (root) {
+                stk.push(root);
+                root = root->left;
+            }
+
+            root = stk.top(); stk.pop();
+
+            if (prev == nullptr) {
+                first = root;
+            } else {
+                prev->right = root;
+            }
+
+            root->left = nullptr;
+            prev = root;
+            root = root->right;
+        }
+
+        return first;
+    }
+};
+//leetcode submit region end(Prohibit modification and deletion)
